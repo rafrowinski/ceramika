c016c1aa99ff088c549e79428f81399e8705800b
rafrowinski <rrowinskipl@gmail.com>
2017-12-10 21:41:28 +0100

    adding prices

--- files.js
+++ files.js
@@ -1,16 +1,19 @@
 const testFolder = '.';
 const fs = require('fs');
 
-var temp = "<div class='tile'><img class='image' src='$2'/><div class='desc'>aaaaaa</div></div>";
-//var i = 1;
-
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
 	  var newFile = file.replace(/ /g, '_');
 	  fs.rename(file, newFile, function(err) {
 			if ( err ) console.log('ERROR: ' + err);
 		});
-	  var foo = temp.replace('$2',newFile);//.replace('$1', i).replace('$1', i++);
+	  var foo = `<a href="${newFile}" class="glightbox-demo" data-glightbox="title:Description Bottom; description: You can set the position of the desciption">
+      <img src="${newFile}" alt="image">
+      <div class="glightbox-desc">
+        <p>Description here</p>
+      </div>
+    </a>`
+    //.replace('$2',newFile);//.replace('$1', i).replace('$1', i++);
     console.log(foo);
   });
 })
